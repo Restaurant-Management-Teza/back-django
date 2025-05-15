74cc7c4409ef2f2582686a9750d98cf387843653
maximc <maxim.cernetchi@cegeka.com>
2025-05-16 00:14:21 +0300

    feat: add сors

--- orders-service/orders/settings.py
+++ orders-service/orders/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "backend",
     "rest_framework",
     'django_filters',
-    'channels'
+    'channels',
+    'corsheaders'
 ]
 
 ASGI_APPLICATION = 'orders.asgi.application'
@@ -71,7 +72,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "orders.urls"
 
